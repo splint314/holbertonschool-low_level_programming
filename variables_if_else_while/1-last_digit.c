41035797d468e1aa063a89800a609fb118af9a2e
splint314 <krigal323@gmail.com>
2025-10-22 12:41:33 +0000

    made an exec who assign a random nulber to the n variable

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		printf("Last digit of %n is %n and is greater than 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %n is %n and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %n is %n and is < than 6 and not 0\n", n, last_digit);
+	}
+	return (0);
+}
