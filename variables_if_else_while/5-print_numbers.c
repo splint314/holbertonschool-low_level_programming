16e9a2a508580b7ab0373595c3f42d70e1860300
splint314 <krigal323@gmail.com>
2025-10-23 13:27:45 +0000

    he had the wrong name

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '10'; c <= '0'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
