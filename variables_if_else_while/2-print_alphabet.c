18845f24112e4d6099eef331e956b8b373b285b2
splint314 <krigal323@gmail.com>
2025-10-24 07:45:01 +0000

    again compiling it

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+    char c;
+    for (c = '0'; c <= '9'; c++)
+        putchar(c);
+    putchar('\n');
+    return (0);
 }
