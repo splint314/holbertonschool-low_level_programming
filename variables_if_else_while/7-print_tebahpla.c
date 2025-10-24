df33d05d3f23ff5db842fec06718353a41f29e59
splint314 <krigal323@gmail.com>
2025-10-24 08:05:06 +0000

    made the script

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'z'; c <= 'c'; c++)
+	{
+		putchar('\n');
+	}
+		return (0);
+}
