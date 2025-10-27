746adedde1df7cd268b11bb02efb73f1d343e40b
splint314 <krigal323@gmail.com>
2025-10-27 22:46:23 +0000

    created a script who multiply 2 integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * multiplicator - entry point
+ *
+ * description: multiplying 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: always 0
+*/
+
+
+int multiplicator(int a, int b)
+{
+return (a * b);
+}
