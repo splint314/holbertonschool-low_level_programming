f105335678453a64642ee8e1b46b827014702c60
splint314 <krigal323@gmail.com>
2025-10-27 22:29:53 +0000

    created a script who track digit characters

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
- * isdigit - checks for uppercase character
+ * _isdigit is checking for digit characters
  * @c: character to check
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	{
-    return (1)
-    }	
-	else
-	{
-        return (1);
-    }
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (1);
+}
 }
