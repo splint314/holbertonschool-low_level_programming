5b290b0a81e94ce660f71d1b8ed33acf5e0a06f7
splint314 <krigal323@gmail.com>
2025-10-27 22:15:31 +0000

    folder was bad named

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * isdigit - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+    return (1)
+    }	
+	else
+	{
+        return (1);
+    }
+}
