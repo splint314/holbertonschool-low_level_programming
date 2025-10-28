09688768c987b13f913eaa4e093f68aa6bf5eaef
splint314 <krigal323@gmail.com>
2025-10-28 00:05:36 +0000

    wrote a script who print 10 times 1 to 14

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_most_numbers(void);
+int _putchar (char c)
+int _isupper(int c);
+int _isdigit(int c);
+int _mul(int a, int b);
+void _print_numbers (void);
+void _print_most_numbers (void);
+void more_numbers(void);
 
-#endif /* MAIN_H */
+#endif
