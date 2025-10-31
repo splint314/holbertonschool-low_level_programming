b55c584507403efe3c84190632b88c65b996efc1
splint314 <krigal323@gmail.com>
2025-10-31 10:30:45 +0000

    added betty code style

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest : destination string
+ * @src : source string
+ *
+ * Return: pointer to dest
+ */
 
 char *_strcat(char *dest, char *src)
 {
