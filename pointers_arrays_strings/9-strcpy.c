941af3b2c0d48a6f143293c9a073f5a501f86c05
splint314 <krigal323@gmail.com>
2025-10-31 09:02:35 +0000

    added betty coding style

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, including the terminating null byte
+ * _strcpy - copies the string pointed to by src,
  * including the terminating null byte, to the buffer pointed to by dest.
  * to the buffer pointed to by dest.
  * @dest : destination string
  * @src : source string
- * 
+ *
  * Return: pointer to dest
  */
-char *_strcpy (char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i = 0;
 
