ced72fd1acd0400d9eea18e4b8d42d13d9f12e7a
splint314 <krigal323@gmail.com>
2025-11-04 10:07:10 +0000

    added an extra space

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -6,14 +6,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+int i;
+int sum1 = 0;
+int sum2 = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		sum1 += *(a + (i * size) + i);
-		sum2 += *(a + (i * size) + (size - i - 1));
-	}
-	printf("%d, %d\n", sum1, sum2);
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + (i * size) + i);
+sum2 += *(a + (i * size) + (size - i - 1));
+}
+printf("%d, %d\n", sum1, sum2);
 }
