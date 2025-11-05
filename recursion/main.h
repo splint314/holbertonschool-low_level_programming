b58a74f9fdd8c20e40303f1bbfba15dcf3afb779
splint314 <krigal323@gmail.com>
2025-11-05 14:53:54 +0000

    recompiled the script

--- recursion/main.h
+++ recursion/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts_recursion(char *s);
-int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
