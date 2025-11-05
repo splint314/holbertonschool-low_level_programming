3b4efea319df9f2f64e9ad61544385e803232be2
splint314 <krigal323@gmail.com>
2025-11-05 14:21:53 +0000

    added more description for the integer function

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,9 +1,13 @@
 #include "main.h"
 /**
- * _print_rev_recursion - prints a string in reverse followed by a new line
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: the base number
  * @y: the exponent
+ *
+ * Return: the value of x raised to the power of y
+ * if y is lower than 0, the function returns -1
  */
+
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
