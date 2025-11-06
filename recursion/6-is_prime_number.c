d27d35e096b65bb5a744d7a1ce80667aa0901097
splint314 <krigal323@gmail.com>
2025-11-06 13:19:35 +0000

    changed all the argumentation

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,14 +1,31 @@
 #include "main.h"
+
 /**
- * is_prime_number - checks if a number is prime
+ * check_prime - checks if n has a divisor starting from i
+ * @n: number to check
+ * @i: current divisor to test
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int check_prime(int n, int i)
+{
+if (i * i > n)      /* on a testé tous les diviseurs utiles */
+return (1);
+if (n % i == 0)     /* trouvé un diviseur */
+return (0);
+return (check_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
  * @n: number to check
  *
  * Return: 1 if n is prime, 0 otherwise
  */
-
 int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return (_sqrt_recursion(n) * _sqrt_recursion(n) == n);
+return (check_prime(n, 2));
 }
