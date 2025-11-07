91071b58323812c63371eb15eceb45b2ae389c2f
splint314 <krigal323@gmail.com>
2025-11-07 16:35:32 +0000

    i'm gonna kill myself

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,7 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-int n1, n2, result;
+int n1 = 0;
+int n2 = 0;
 
 if (argc != 3)
 {
@@ -20,8 +21,8 @@ return (1);
 
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-result = n1 * n2;
 
-printf("%d\n", result);
+printf("%d\n", n1 *n2);
+
 return (0);
 }
