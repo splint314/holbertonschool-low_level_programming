2de047834f5a9a9fb53f513b284c8f7e3e47d312
splint314 <krigal323@gmail.com>
2025-11-14 15:14:02 +0000

    he haven't compiled

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - defines a dog with basic information
+ * struct dog - dog's basic information
  * @name: dog's name
  * @age: dog's age
- * @owner: owner's name
+ * @owner: dog's owner
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
-#endif
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /* DOG_H */
