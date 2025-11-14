3b4d1d14010aee3eb23d0a9d792d0551654aecc2
splint314 <krigal323@gmail.com>
2025-11-14 15:06:43 +0000

    structed a new type of dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - dog's basic information
+ * struct dog - defines a dog with basic information
  * @name: dog's name
  * @age: dog's age
- * @owner: dog's owner
+ * @owner: owner's name
  */
 struct dog
 {
@@ -13,11 +12,4 @@ char *name;
 float age;
 char *owner;
 };
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif /* DOG_H */
+#endif
