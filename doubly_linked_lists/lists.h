6d472760235132e2af5962ee5bcc3b6b9b9d234e
splint314 <krigal323@gmail.com>
2025-12-05 11:04:36 +0100

    repush1

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>  /* pour size_t */
+#include <stddef.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -21,5 +21,6 @@ typedef struct dlistint_s
 /* Prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
