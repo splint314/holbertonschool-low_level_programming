680ab6dd81671a4034a0f26d76d1c86e2fc2183d
splint314 <krigal323@gmail.com>
2026-02-24 15:45:44 +0100

    feat: made a program who printss numbers from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+
+
+putchar(c);
+
+putchar('\n');
+return (0);
+}
