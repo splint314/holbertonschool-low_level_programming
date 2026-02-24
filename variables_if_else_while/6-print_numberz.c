bc7820b2c9f93c271e1939618b917fc757c6c624
splint314 <krigal323@gmail.com>
2026-02-24 15:57:34 +0100

    feat: made a program who prints all single numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0 to 9 using putchar only twice
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+
+putchar('\n');
+
+return (0);
+}
