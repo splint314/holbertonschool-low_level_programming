ef4dea43c8f999a671d700870d9067f760c5eee7
splint314 <krigal323@gmail.com>
2026-02-24 15:13:17 +0100

    fix: replaced an integer by another

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,26 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- *main: prints the last digit of a number and if he's +or- than certain numbers
-*/
+/**
+ * main - prints the last digit of a random number
+ *
+ *Return: Always 0
+ */
 
 int main(void)
 {
 int n;
+int last_digit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
+last_digit = n % 10;
+
+if (last_digit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
-else if (n == 0)
+else if (last_digit == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
-{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 
 return (0);
