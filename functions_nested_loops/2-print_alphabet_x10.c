250ef209a06d5e2f81b755a8409f4d711ad7e8bc
splint314 <krigal323@gmail.com>
2026-02-25 15:29:46 +0100

    fix: removed all whitespace

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  * Return: void
  */
 void print_alphabet_x10(void)
-{	
+{
 int i, j;
 for (i = 0; i < 10; i++)
-{	
+{
 for (j = 'a'; j <= 'z'; j++)
-{ 
+{
 _putchar(j);
-} 
+}
 _putchar('\n');
 }
 }
