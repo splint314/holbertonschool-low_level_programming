eeccddb0ef0ac2946a6bf3705e8c3bb318eb53c1
splint314 <krigal323@gmail.com>
2026-02-26 14:37:57 +0100

    fix: added more descriptions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,9 +3,13 @@
 /**
  * print_sign - prints the sign of a number.
  * @n: The number to be checked.
+ * Return: 1 and prints + if n is greater than zero
+ * -1 and prints - if n is less than zero,
+ * 0 and prints 0 if n is zero,
  */
 
 int print_sign(int n)
+
 {
 if (n > 0)
 {_putchar('+');
