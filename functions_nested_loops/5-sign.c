5f0df8f12e7266f065ea52923116b1314b2a9f2b
splint314 <krigal323@gmail.com>
2026-02-26 14:35:22 +0100

    feat: made a program who prints the sign of a number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ * @n: The number to be checked.
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{_putchar('+');
+return (1);
+}
+else if (n == 0)
+{_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
