ea89f4c0cf52bf9076c59a91433affdc497ccc34
splint314 <krigal323@gmail.com>
2026-02-27 09:03:22 +0000

    feat: made a program who prints the 9 time tables from 1 to 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting by 0
+ */
+
+void times_table(void)
+{
+int i, j, r;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)		{
+r = i * j;
+
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+if (r < 10)
+_putchar(' ');
+}
+
+if (r >= 10)
+_putchar((r / 10) + '0');
+
+_putchar((r % 10) + '0');
+}
+_putchar('\n');
+}
+}
