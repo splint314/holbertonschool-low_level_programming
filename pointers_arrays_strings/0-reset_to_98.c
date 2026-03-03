2dec06e2fce53ad28cfeffd2cef31460c7b9704e
splint314 <splint314>
2026-03-03 13:43:44 +0000

    fix: added betty coding style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
-/* * n  = l'adresse de la boîte (ex: 0x7ffd...)
-* *n = le contenu de la boîte
+/*
+* * n  = adress of the integer
+* *n = integer's content
 */
 
 void reset_to_98(int *n)
 
 {
-*n = 98; 
+*n = 98;
 }
