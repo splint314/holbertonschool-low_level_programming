c0744fd37a736535537bd6c1fe9f58f8743ad1a9
splint314 <krigal323@gmail.com>
2026-03-05 16:04:21 +0100

    feat: made a function who reverse an array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_array - reverses an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+    int i;
+    int temp;
+
+    i = 0;
+    while (i < n / 2)
+    {
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
+        i++;
+    }
+}
