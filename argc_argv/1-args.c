825adf92daecbbbe975caad892876dcafa0c41bf
splint314 <krigal323@gmail.com>
2026-03-08 18:37:45 +0100

    fix: added stdio.h

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,14 @@
-#include "holberton.h"
+#include <stdio.h>
+
 /**
- * main - prints the number of arguments passed to the program
- * @argc: the number of arguments
- * @argv: the array of arguments
+ * main - prints the number of arguments passed to it
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: 0 on success
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-
 (void)argv;
 
 printf("%d\n", argc - 1);
