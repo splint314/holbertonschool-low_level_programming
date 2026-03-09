32cc16341c16ab797f73e66d95d65af851967b21
splint314 <krigal323@gmail.com>
2026-03-09 22:34:41 +0100

    push for task 1&2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int len, i;
+
+if (str == NULL)
+return (NULL);
+
+len = strlen(str);
+dup = malloc((len + 1) * sizeof(char));
+if (dup == NULL)
+return (NULL);
+
+for (i = 0; i <= len; i++)
+dup[i] = str[i];
+
+return (dup);
+}
