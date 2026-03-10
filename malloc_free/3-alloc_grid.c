f2a643fca9f88c4903f1d5337e05260c41870cc4
splint314 <krigal323@gmail.com>
2026-03-10 14:45:14 +0000

    feat: made a function that return a pointer to a d arrray

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - crée une grille 2D d'entiers initialisée à 0
+ * @width: largeur de la grille (colonnes)
+ * @height: hauteur de la grille (lignes)
+ *
+ * Return: pointeur vers la grille 2D, ou NULL si échec ou taille <= 0
+ */
+int **alloc_grid(int width, int height)
+{
+int **grid;
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+grid = malloc(sizeof(int *) * height);
+
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+
+if (grid[i] == NULL)
+		{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+
+return (grid);
+}
