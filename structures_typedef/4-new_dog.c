96d8b01d4c066a8747a09eefa6c071df3115c2ef
splint314 <krigal323@gmail.com>
2026-03-16 17:24:01 +0100

    feat: created a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to new dog, or NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+
+return (dog);
+}
