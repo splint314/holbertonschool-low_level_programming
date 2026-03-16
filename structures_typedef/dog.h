639ec0fddacb1d2669790919a9f4978bbefc20a7
splint314 <krigal323@gmail.com>
2026-03-16 12:31:21 +0100

    feat: made a structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This structure holds information about a dog, including name
+ * age, and owner.
+ */
+
+struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
