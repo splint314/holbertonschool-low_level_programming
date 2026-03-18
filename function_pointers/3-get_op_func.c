4daa539815ceb594ac225a826f64873d078d9ff8
splint314 <krigal323@gmail.com>
2026-03-18 09:03:34 +0100

    feat: made a calculator

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * @s: operator
+ *
+ * Return: pointer to function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+
+while (ops[i].op != NULL && s[0] != ops[i].op[0])
+i++;
+
+if (ops[i].op != NULL)
+return (ops[i].f);
+
+return (NULL);
+}
