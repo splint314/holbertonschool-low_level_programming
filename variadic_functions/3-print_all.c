6d78c1c9ffe8b34b7ccaf70955142503444b3a81
splint314 <krigal323@gmail.com>
2026-03-20 16:08:29 +0100

    feat: made a function who prints some texts

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+*
+* Return: nothing
+*/
+
+void print_all(const char * const format, ...)
+
+{
+va_list args;
+unsigned int i = 0, j = 0;
+char *str;
+
+va_start(args, format);
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s", str);
+break;
+default:
+j++;
+break;
+}
+if (format[i + 1] && j == 0)
+printf(", ");
+i++;
+}
+printf("\n");
+va_end(args);
+}
