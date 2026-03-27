1c9b43378f6363a0e7d5a8a31eced46878e0c514
splint314 <krigal323@gmail.com>
2026-03-27 17:40:49 +0100

    fix: reworked lists.h

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,8 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
 * add_node - adds a new node at the beginning of a list_t list.
 * @head: double pointer to the head of the list.
