23df7987d86354daea1d358756ae4b48448464fb
splint314 <krigal323@gmail.com>
2026-03-31 14:48:02 +0000

    saving

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,17 @@
-#include lists.h
-#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 
 {
+size_t count = 0;
+
 while (h != NULL)
 {
-printf("%d ", h->n);
+printf("%d\n", h->n);
 h = h->next;
-}
-printf("\n");
+count++;
 }
 
 return count;
+}
