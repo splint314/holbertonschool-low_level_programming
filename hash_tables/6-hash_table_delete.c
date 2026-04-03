0138dd8eaa0a75c05bef70b846961cc4906a1b6f
splint314 <krigal323@gmail.com>
2026-04-03 22:41:38 +0200

    feat: deleted an hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *tmp;
+hash_node_t *next;
+if (ht == NULL)
+return;
+for (i = 0; i < ht->size; i++)
+{
+tmp = ht->array[i];
+while (tmp)
+{
+next = tmp->next;
+free(tmp->key);
+free(tmp->value);
+free(tmp);
+tmp = next;
+}
+}
+free(ht->array);
+free(ht);
+}
